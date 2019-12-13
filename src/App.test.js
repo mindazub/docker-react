12a6cb4194c15e5f30775ff46d3c3d6fe08701aa
Mindaugas Azubalis <mindaugas.azubalis@gmail.com>
2019-12-13 16:17:30 +0200

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,16 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/i was changed/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/problem/i);
+  expect(linkElement).toBeInTheDocument();
+});
